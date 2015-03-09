3beff5d1e3c363ec3867d89cb6c5f8ccb4fc7929
Julio Foulquié <jfoulquie@gmail.com>
2015-03-09 23:47:30 +0100

    Add first file structure.

---
+++ src/j3j5/ControlMyFollowers.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *
+ * @author Julio Foulquié
+ * @version 0.1.0
+ *
+ *
+ */
+
+namespace j3j5;
+
+class ControlMyFollowers {
+
+	private $api;
+
+	public function __construct() {
+		$this->api = new TwitterApio();
+	}
+
+	public function get_all_my_followers() {
+
+	}
+
+	public function get_all_my_following() {
+
+	}
+
+}
